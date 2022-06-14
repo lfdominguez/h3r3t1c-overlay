848afce0d55af96e75bf4f290046ee7285698ec8
Luis Felipe Domínguez Vega <ldominguezvega@gmail.com>
2022-06-13 20:31:30 -0400

    Fix typo
    Signed-off-by: Luis Felipe Domínguez Vega <ldominguezvega@gmail.com>

--- default.nix
+++ default.nix
@@ -1,5 +1,5 @@
 self: super: {
-  nvidia_x11 = (super.nvidia_x11.override ()).overrideAttrs
+  nvidia_x11 = (super.nvidia_x11.override {}).overrideAttrs
     (oldAttrs: red {
       pname = "nvidia";
       src = /home/luis/Downloads/NVIDIA-Linux-x86_64-515.48.07.run;
