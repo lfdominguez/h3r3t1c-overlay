8eb0faf981070aa1900cb8cebd213826d7459689
Luis Felipe Domínguez Vega <ldominguezvega@gmail.com>
2022-06-13 20:02:23 -0400

    Add nvidia_x11 overlay to use local .run file
    Signed-off-by: Luis Felipe Domínguez Vega <ldominguezvega@gmail.com>

---
+++ default.nix
@@ -0,0 +1,7 @@
+self: super: {
+  nvidia_x11 = (super.nvidia_x11.override ()).overrideAttrs
+    (oldAttrs: red {
+      pname = "nvidia";
+      src = /home/luis/Downloads/NVIDIA-Linux-x86_64-515.48.07.run;
+    });
+}
