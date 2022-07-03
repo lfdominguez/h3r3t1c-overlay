4e8b36899bbf601f54ee8eee4251e32bde37dce4
Luis Felipe Domínguez Vega <ldominguezvega@gmail.com>
2022-07-03 16:41:53 -0400

    Add scope linuxPackages nvidia_x11
    Signed-off-by: Luis Felipe Domínguez Vega <ldominguezvega@gmail.com>

--- default.nix
+++ default.nix
@@ -1,6 +1,12 @@
 self: super: {
-  linuxPackages_latest = super.linuxPackages_latest.extend (lpself: lpsuper: {
-      nvidia_x11 = super.linuxPackages_latest.nvidia_x11.overrideAttrs (oldAttrs: rec {
+  linuxPackages_5_15 = super.linuxPackages_5_15.extend (lpself: lpsuper: {
+      nvidia_x11 = super.linuxPackages_5_15.nvidia_x11.overrideAttrs (oldAttrs: rec {
+        src = /home/luis/Downloads/NVIDIA-Linux-x86_64-515.48.07.run;
+        useGLVND = true;
+      });
+  });
+  linuxPackages = super.linuxPackages.extend (lpself: lpsuper: {
+      nvidia_x11 = super.linuxPackages.nvidia_x11.overrideAttrs (oldAttrs: rec {
         src = /home/luis/Downloads/NVIDIA-Linux-x86_64-515.48.07.run;
         useGLVND = true;
       });
