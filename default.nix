252347cf43159ccc6e5b3469859f597921849945
Luis Felipe Domínguez Vega <ldominguezvega@gmail.com>
2022-07-03 16:58:33 -0400

    Remove scope linuxPackages nvidiaPackages
    Signed-off-by: Luis Felipe Domínguez Vega <ldominguezvega@gmail.com>

--- default.nix
+++ default.nix
@@ -4,17 +4,9 @@ self: super: {
         src = /home/luis/Downloads/NVIDIA-Linux-x86_64-515.48.07.run;
         useGLVND = true;
       });
-      nvidiaPackages = super.linuxPackages_5_15.nvidiaPackages.overrideAttrs (oldAttrs: rec {
-        src = /home/luis/Downloads/NVIDIA-Linux-x86_64-515.48.07.run;
-        useGLVND = true;
-      });
   });
   linuxPackages = super.linuxPackages.extend (lpself: lpsuper: {
       nvidia_x11 = super.linuxPackages.nvidia_x11.overrideAttrs (oldAttrs: rec {
-        src = /home/luis/Downloads/NVIDIA-Linux-x86_64-515.48.07.run;
-        useGLVND = true;
-      });
-      nvidiaPackages = super.linuxPackages.nvidiaPackages.overrideAttrs (oldAttrs: rec {
         src = /home/luis/Downloads/NVIDIA-Linux-x86_64-515.48.07.run;
         useGLVND = true;
       });
