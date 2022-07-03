b88bf5d601a87377c8ba884a717dffb67d030e5c
Luis Felipe Domínguez Vega <ldominguezvega@gmail.com>
2022-07-02 20:17:01 -0400

    Force nvidia-x11 package name
    Signed-off-by: Luis Felipe Domínguez Vega <ldominguezvega@gmail.com>

--- default.nix
+++ default.nix
@@ -1,5 +1,5 @@
 self: super: {
-  nvidia_x11 = (super.linuxKernel.packages.linux_5_15.nvidia_x11.override {}).overrideAttrs
+  linuxkernel.packages.linux_5_15.nvidia_x11 = (super.linuxKernel.packages.linux_5_15.nvidia_x11.override {}).overrideAttrs
     (oldAttrs: rec {
       src = /home/luis/Downloads/NVIDIA-Linux-x86_64-515.48.07.run;
     });
