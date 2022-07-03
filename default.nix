ccae1607fb6de962e61c4935b4b2c42ce2c3cee3
Luis Felipe Domínguez Vega <ldominguezvega@gmail.com>
2022-07-02 21:28:00 -0400

    Change method to override nvidia_x11
    Signed-off-by: Luis Felipe Domínguez Vega <ldominguezvega@gmail.com>

--- default.nix
+++ default.nix
@@ -1,6 +1,6 @@
 self: super: {
-  linuxkernel.packages.linux_5_15.nvidia_x11 = (super.linuxKernel.packages.linux_5_15.nvidia_x11.override {}).overrideAttrs
-    (oldAttrs: rec {
+  nvidia_x11 = super.nvidia_x11.overrideAttrs
+    (old: {
       src = /home/luis/Downloads/NVIDIA-Linux-x86_64-515.48.07.run;
     });
 }
