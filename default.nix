ceec5795e3038091984f86839fc9fe291ff6348a
Luis Felipe Domínguez Vega <ldominguezvega@gmail.com>
2022-07-03 16:22:43 -0400

    Add new nvidia-x11-515.48.07-5.15.43 test for nvidia-x11 module build
    Signed-off-by: Luis Felipe Domínguez Vega <ldominguezvega@gmail.com>

--- default.nix
+++ default.nix
@@ -1,14 +1,5 @@
 self: super: {
-  
-  nvidia_x11 = super.nvidia_x11.overrideAttrs
-    (old: {
-      src = /home/luis/Downloads/NVIDIA-Linux-x86_64-515.48.07.run;
-    });
-  "nvidia-x11-515.48.07-5.15.43" = super.nvidia_x11.overrideAttrs
-    (old: {
-      src = /home/luis/Downloads/NVIDIA-Linux-x86_64-515.48.07.run;
-    });
-  linuxPackages_5_15.nvidia_x11 = super.linuxPackages_5_15.nvidia_x11.overrideAttrs
+  linuxPackages_latest.nvidia_x11 = super.linuxPackages_latest.nvidia_x11.overrideAttrs
     (oldAttrs: rec {
       src = /home/luis/Downloads/NVIDIA-Linux-x86_64-515.48.07.run;
       useGLVND = true;
